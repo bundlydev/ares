652fad68b4853576c849be258e5d15a9a8c2c825
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2024-02-29 17:29:32 -0600

    add missing useCandidActor hook

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from "./useActor";
 export * from "./useAuth";
+export * from "./useCandidActor";
 export * from "./useClient";
 export * from "./useCurrentProvider";
 export * from "./useRestActor";
