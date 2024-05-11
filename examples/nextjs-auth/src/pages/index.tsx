fe1b0d33ac8943f39f7468e022231840a0a351b2
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2024-05-11 14:52:18 -0600

    example renamed

--- examples/nextjs-auth/src/pages/index.tsx
+++ examples/nextjs-auth/src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function IcConnectPage() {
 
   return (
     <div className="max-w-4xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-4">IC Connect</h1>
+      <h1 className="text-3xl font-bold mb-4">Ares Connect</h1>
       <div className="flex justify-between mb-4">
         <InternetIdentityButton onError={onLoginError} />
       </div>
